4304c349c137e58ed6ba6fef8a57ada66b3c1307
mystic moon <162459275+meditativetube@users.noreply.github.com>
2024-07-13 00:32:23 +0545

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementsByClassName("nav");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
